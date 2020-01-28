ae31e3370b27a0caa4ea978a7603ce86591382f2
Mason Ashment <mashment.iu.edu>
2020-01-28 14:57:28 -0500

    added name to owner

--- app/src/main/java/com/example/curbside/Owner.java
+++ app/src/main/java/com/example/curbside/Owner.java
@@ -2,6 +2,8 @@ package com.example.curbside;
 
 public class Owner extends Vendor {
 
+    private String name;
+
     public Owner(User user) {
         super(user);
         this.setPermissions(3);
