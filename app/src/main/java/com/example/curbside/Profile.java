50df0f64fd54cad91f026480de77709e3abe4cdc
Nick Van Fossen <nvanfoss@imail.iu.edu>
2020-01-30 09:10:19 -0500

    Ready for instructor meeting

--- app/src/main/java/com/example/curbside/Profile.java
+++ app/src/main/java/com/example/curbside/Profile.java
@@ -10,5 +10,6 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
     }
 }
