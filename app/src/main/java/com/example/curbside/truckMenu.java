a4b54148cb9aad4db6b2b8099201fb662c241bca
Augustus Serrani <gserrani@iu.edu>
2020-02-09 16:39:46 -0500

    truck menu/notifications pages added. color palette updated

---
+++ app/src/main/java/com/example/curbside/truckMenu.java
@@ -0,0 +1,14 @@
+package com.example.curbside;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class truckMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_truck_menu);
+    }
+}
