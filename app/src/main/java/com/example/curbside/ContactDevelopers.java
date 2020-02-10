a9ec949ff08cfa2a9c79a30120f1f1d5381bf9a4
Augustus Serrani <gserrani@iu.edu>
2020-02-10 15:13:42 -0500

    contact developers page, drawables updates

---
+++ app/src/main/java/com/example/curbside/ContactDevelopers.java
@@ -0,0 +1,14 @@
+package com.example.curbside;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactDevelopers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_developers);
+    }
+}
