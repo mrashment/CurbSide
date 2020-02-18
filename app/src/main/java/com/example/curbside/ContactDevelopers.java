036b378a7982d2591a547ee7cba750a739e50d84
Nick Van Fossen <nvanfoss@imail.iu.edu>
2020-02-18 12:54:18 -0500

    Trying to solve gmail link

--- app/src/main/java/com/example/curbside/ContactDevelopers.java
+++ app/src/main/java/com/example/curbside/ContactDevelopers.java
@@ -14,8 +14,11 @@ public class ContactDevelopers extends AppCompatActivity {
     }
 
     Intent intent = new Intent(Intent.ACTION_SEND);
-intent.setType("text/plain"); // send email as plain text
-intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"some@email.address"});
+    intent.setType("text/plain"); // send email as plain text
+    intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"some@email.address"});
+    intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
+    intent.putExtra(Intent.EXTRA_TEXT, "mail body");
+    startActivity(Intent.createChooser(intent, ""));
 
 
 
