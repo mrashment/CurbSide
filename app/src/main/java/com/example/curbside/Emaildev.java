22355cdb526d4f2f24f25cd44bcbe9fcb9b7640c
Nick Van Fossen <nvanfoss@imail.iu.edu>
2020-02-19 13:17:32 -0500

    Staring to create the layout for email dev

---
+++ app/src/main/java/com/example/curbside/Emaildev.java
@@ -0,0 +1,14 @@
+package com.example.curbside;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Emaildev extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emaildev);
+    }
+}
