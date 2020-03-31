29af9b6b28d066d09bc72ad61535d072ad0b74e3
Augustus Serrani <gserrani@iu.edu>
2020-03-31 05:01:35 -0400

    truck name/hours context imported to edit truck interface.

--- app/src/main/java/com/example/curbside/EditTruckInterface.java
+++ app/src/main/java/com/example/curbside/EditTruckInterface.java
@@ -5,18 +5,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditTruckInterface extends AppCompatActivity {
     private Button backToEditTrucks, applyTruckChange;
+    private Truck truck;
+    private View editTruckName, editTruckHours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_menu_interface);
+        Intent intent = getIntent();
+        truck = (Truck)intent.getSerializableExtra("com.example.curbside.truck");
 
         backToEditTrucks = findViewById(R.id.backToEditTrucks);
         applyTruckChange = findViewById(R.id.applyTruckChange);
+        editTruckHours = findViewById(R.id.editTruckHours);
+        editTruckName = findViewById(R.id.editTruckName);
 
         backToEditTrucks.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +33,5 @@ public class EditTruckInterface extends AppCompatActivity {
             }
         });
     }
+
 }
