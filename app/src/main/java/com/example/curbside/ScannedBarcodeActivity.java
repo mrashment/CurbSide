e8ed01a681708ad556902b3b072f26188681d374
Nick Van Fossen <nvanfoss@imail.iu.edu>
2020-04-01 15:09:50 -0400

    Requesting camera permission

--- app/src/main/java/com/example/curbside/ScannedBarcodeActivity.java
+++ app/src/main/java/com/example/curbside/ScannedBarcodeActivity.java
@@ -21,3 +21,13 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.IOException;
 
+public class ScannedBarcodeActivity extends AppCompatActivity {
+
+    SurfaceView surfaceView;
+    TextView txtBarcodeValue;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
+    Button btnAction;
+    String intentData = "";
+    boolean isEmail = false;
