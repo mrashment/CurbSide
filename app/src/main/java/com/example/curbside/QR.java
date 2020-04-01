814f1358b517c08b7854eeddfae2defd2cee0892
Nick Van Fossen <nvanfoss@imail.iu.edu>
2020-04-01 15:05:10 -0400

    Creating the public class for main activity

--- app/src/main/java/com/example/curbside/QR.java
+++ app/src/main/java/com/example/curbside/QR.java
@@ -6,4 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnTakePicture, btnScanBarcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
 
