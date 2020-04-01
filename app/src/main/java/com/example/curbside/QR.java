616cb204a5f94ce543d1d35cb95c89c12e251dbd
Nick Van Fossen <nvanfoss@imail.iu.edu>
2020-04-01 15:04:00 -0400

    Importing necessary moduales

---
+++ app/src/main/java/com/example/curbside/QR.java
@@ -0,0 +1,9 @@
+package com.example.curbside;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
---
+++ app/src/main/java/com/example/curbside/ScannedBarcodeActivity.java
@@ -0,0 +1,14 @@
+package com.example.curbside;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScannedBarcodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_picture);
+    }
+}
