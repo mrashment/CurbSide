5df2ca68097895b076661385582ecb13ba23c039
Mason Ashment <mashment@iu.edu>
2020-04-08 13:20:23 -0400

    added url to dbconnection

--- app/src/main/java/com/example/curbside/DbConnection.java
+++ app/src/main/java/com/example/curbside/DbConnection.java
@@ -21,6 +21,7 @@ public class DbConnection {
     public static final String USER_ADD = "https://cgi.sice.indiana.edu/~team59/useradd.php"; // adds user to db
     public static final String NEARBY_TRUCKS = "https://cgi.sice.indiana.edu/~team59/nearbytrucks.php";
     public static final String FAVORITE_TRUCKS = "https://cgi.sice.indiana.edu/~team59/favoritetrucks.php";
+    public static final String COMPANY_TRUCKS = "https://cgi.sice.indiana.edu/~team59/companytrucks.php";
 
     public static DbConnection getInstance() {
         return db;
