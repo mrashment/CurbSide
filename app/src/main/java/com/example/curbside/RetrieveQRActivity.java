418d6df1818bd1515d9b756a36e18ab77fb013a3
Mason Ashment <mashment@iu.edu>
2020-04-16 18:19:10 -0400

    vendor can download qr code to phone

---
+++ app/src/main/java/com/example/curbside/RetrieveQRActivity.java
@@ -0,0 +1,99 @@
+package com.example.curbside;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class RetrieveQRActivity extends AppCompatActivity {
+    private static final String TAG = "RetrieveQRActivity";
+
+    private Button downloadButton;
+    private static final int WRITE_PERMISSION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retrieve_q_r);
+
+        downloadButton = findViewById(R.id.downloadButton);
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                downloadQR();
+            }
+        });
+    }
+
+    private void downloadQR() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},WRITE_PERMISSION);
+
+        } else {
+            Bitmap bm;// = BitmapFactory.decodeResource(getResources(),R.drawable.curbside_qr);
+            bm = ((BitmapDrawable) getResources().getDrawable(R.drawable.curbside_qr)).getBitmap();
+            String extStorageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
+            String state = Environment.getExternalStorageState();
+            if (!Environment.MEDIA_MOUNTED.equals(state)) {
+                Toast.makeText(this, "Something went wrong.", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            File file = new File(extStorageDirectory, "curbside_qr.PNG");
+            Log.d(TAG, "downloadQR: " + extStorageDirectory);
+
+
+            FileOutputStream outputStream;
+            try {
+                outputStream = new FileOutputStream(file);
+                bm.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                outputStream.flush();
+                outputStream.close();
+                Toast.makeText(this,"QR code downloaded",Toast.LENGTH_LONG).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case WRITE_PERMISSION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    downloadQR();
+                } else {
+                    Toast.makeText(this,"External storage permissions required to download QR code",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+}
