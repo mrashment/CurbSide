66a3bee02856926179527766edc29745abf79219
Nick Van Fossen <nvanfoss@imail.iu.edu>
2020-04-19 22:50:29 -0400

    user privacy ready to test

--- app/src/main/java/com/example/curbside/UserPrivacy.java
+++ app/src/main/java/com/example/curbside/UserPrivacy.java
@@ -4,4 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UserPrivacy extends AppCompatActivity {}
+public class UserPrivacy extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_privacy);
+
+    }
+}
+
