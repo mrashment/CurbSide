28078258752268c8a653ab520e57aeceaabb0bd6
Augustus Serrani <gserrani@iu.edu>
2020-04-19 20:33:17 -0400

    icons updated

--- app/src/main/java/com/example/curbside/AddMenuItemActivity.java
+++ app/src/main/java/com/example/curbside/AddMenuItemActivity.java
@@ -27,7 +27,6 @@ public class AddMenuItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_menu_item);
 
-
         newItemName = findViewById(R.id.newItemName);
         newItemDescription = findViewById(R.id.newItemDescription);
         newItemPrice = findViewById(R.id.newItemPrice);
